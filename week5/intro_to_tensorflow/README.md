19e6f8b4b409036da56c4527382b3bfbba2a2181
Tom Martin <tom@tpgmartin.com>
2017-02-25 23:54:11 +0000

    Begin week 5, intro to tensorflow

---
+++ week5/intro_to_tensorflow/README.md
@@ -0,0 +1,18 @@
+# Introduction to Tensor Flow
+
+## Hello, Tensor World!
+
+TensorFlow's API is built around the idea of a computational graph.
+
+A "TensorFlow Session" is an environment for running a graph. The session is in 
+charge of allocating the operatiosn to GPUs/CPUs
+
+## TensorFlow Math
+
+[TensorFlow math](https://www.tensorflow.org/api_guides/python/math_ops)
+
+## Minising Cross Entropy
+
+Find loss: average cross-entropy 
+
+
---
+++ week5/intro_to_tensorflow/one_hot_encoding.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn import preprocessing
+
+# Example labels
+labels = np.array([1,5,3,2,1,4,2,1,3])
+
+# Create the encoder
+lb = preprocessing.LabelBinarizer()
+
+# Here the encoder finds the classes and assigns one-hot vectors 
+lb.fit(labels)
+
+# And finally, transform the labels into one-hot encoded vectors
+lb.transform(labels)
+# >>> array([[1, 0, 0, 0, 0],
+#            [0, 0, 0, 0, 1],
+#            [0, 0, 1, 0, 0],
+#            [0, 1, 0, 0, 0],
+#            [1, 0, 0, 0, 0],
+#            [0, 0, 0, 1, 0],
+#            [0, 1, 0, 0, 0],
+#            [1, 0, 0, 0, 0],
+#            [0, 0, 1, 0, 0]])
